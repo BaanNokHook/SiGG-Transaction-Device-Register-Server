ef3f08e18309a61367d9e9f99518eae625edc40c
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-10-11 09:23:20 +0700

    Clean-Code-Develop-metrics.go

---
+++ pkg/api/metrics/metrics.go
@@ -0,0 +1,27 @@
+package metrics
+
+import (
+	"github.com/prometheus/client_golang/prometheus"
+)
+
+var (
+	FetchProcessTime = prometheus.NewGauge(prometheus.GaugeOpts{
+		Name: "eureka_fetch_nanoseconds",  
+		Help: "process time per api client fetch",   
+	})   
+	RequestCount = prometheus.NewCounterVec( 
+		prometheus.CounterOpts{
+			Name: "eureka_ge_total", 
+			Help: "Total of the api fetch from client.",  
+		},   
+		[]string{"path"},  
+	)
+)
+
+func init() {  
+	prometheus.MustRegister(RequestCount)  
+	prometheus.MustRegister(FetchProcessTime)   
+}  
+
+
+
