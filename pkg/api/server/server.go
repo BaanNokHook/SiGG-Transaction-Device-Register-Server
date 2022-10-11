a568550e26281c1c27e72baef638d5e0e8e0b6fb
SiGGFedCJKP <baannokhooksg@gmail.com>
2022-10-11 13:50:46 +0700

    Clean-Code-Develop-server.go

---
+++ pkg/api/server/server.go
@@ -0,0 +1,47 @@
+package server
+
+import (
+	"github.com/choerodon/go-register-server/pkg/api/router"
+	"net/http"
+
+	"github.com/golang/glog"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
+)
+
+type RegisterServer struct { 
+	Config *RegisterServerOptions  
+}   
+
+type PreparedRegisterServer struct { 
+	*RegisterServer    
+}   
+
+func CreateRegisterServer(options *RegisterServerOptions) *RegisterServer {  
+	s := &RegisterServer{
+		Config: options,    
+	}  
+
+	return s 
+}
+
+func (s *registerServer) PrepareRun() *PreparedRegisterServer {  
+	return &PreparedRegisterServer{s}
+}  
+
+
+func (s *PreparedRegisterServer) Run(stopCh <- chan struct{}) error {  
+
+	router.Register()  
+
+	http.Handle("/metrics", promhttp.Handler())  
+
+	go func() {
+		glog.Fatal(http.ListenAndServe(":8000", nil))   
+	}() 
+
+	glog.Info("Started server")  
+	<-stopCh
+	glog.Info("Shutting down server")   
+
+	return nil
+}
